d31f13857cf2acb1156639f63773aee2cb51bc11
projectacht <projectacht.rm@gmail.com>
2014-01-20 15:17:57 +0100

    Bump version number

--- asu/__init__.py
+++ asu/__init__.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 components = 'major, minor, micro, releaselevel, serial'
-values = (0, 9, 0, 'beta', 0)
+values = (0, 10, 0, 'beta', 0)
 __version_info__ = namedtuple('__version_info__', components)(*values)
 __version__ = (str(__version_info__.major) + '.' +
                str(__version_info__.minor) + ' ' +
